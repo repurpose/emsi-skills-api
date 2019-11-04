05d618aad00daa022e9f456e85bf3bffa0657cc8
Bill Broughton <bill@billbroughton.me>
2019-11-03 22:11:46 -0500

    Essential endpoints

---
+++ index.js
@@ -0,0 +1,7 @@
+module.exports = {
+  docs: require("./lib/api/docs"),
+  skillTypes: require("./lib/api/skill-types"),
+  skills: require("./lib/api/skills"),
+  status: require("./lib/api/status"),
+  versions: require("./lib/api/versions")
+};
